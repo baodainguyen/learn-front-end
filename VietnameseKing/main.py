5b11907b6816f00a55d0d3e0769e7603fb8893ab
nguyen.ba.dai <nguyen.ba.dai@kloon.vn>
2021-10-26 09:49:54 +0700

    learn vnms

--- VietnameseKing/main.py
+++ VietnameseKing/main.py
@@ -1,12 +1,24 @@
 import mmap
 
-searchText = 'aá'
+searchText = 'á '
 
 def check():
     found = False
     with open('words.txt', 'rb', 0) as f, \
          mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ) as s:
-        if s.find(searchText.encode()) != -1:
+        bText = searchText.encode()
+        i = s.find(bText)
+        z = s.rfind(bText)
+        print(i, z)
+        while (i > -1 and i <= z):
+            j = s.find(b'"', i)
+            print(s[i:j].decode("utf-8") )
+            i = s.find(bText, j)
+
+        if i != -1:
+            j = s.find(b'"', i)
+            print(s[i:j].decode("utf-8") )
+            print(j)
             return True
     return False
 

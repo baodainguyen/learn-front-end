80c7e0eccabaa78257772239a47ef46d2a3d5816
nguyen.ba.dai <nguyen.ba.dai@kloon.vn>
2021-10-26 13:49:36 +0700

    learning py

--- VietnameseKing/main.py
+++ VietnameseKing/main.py
@@ -1,28 +1,79 @@
 import mmap
+import re
 
-searchText = 'mụ'
+fileTxt = 'words.txt'
+text = 'l/i/n/h/m/ụ/c'
+keyWord = 'ụ'
 
-def check():
-    with open('words.txt', 'rb', 0) as f, \
+
+def removeSlash(text):
+    return text.replace('/', '')
+
+sText = removeSlash(text)
+
+def ignore(w, ignoreChar):
+    rr = []
+    a = 'à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ'.replace(ignoreChar, '').replace('||', '|')
+    rr.append(a)
+    o = 'ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ'.replace(ignoreChar, '').replace('||', '|')
+    rr.append(o)
+    e = 'è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ'.replace(ignoreChar, '').replace('||', '|')
+    rr.append(e)
+    u = 'ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ'.replace(ignoreChar, '').replace('||', '|')
+    rr.append(u)
+    i = 'ì|í|ị|ỉ|ĩ'.replace(ignoreChar, '').replace('||', '|')
+    rr.append(i)
+    y = 'ỳ|ý|ỵ|ỷ|ỹ'.replace(ignoreChar, '').replace('||', '|')
+    rr.append(y)
+    d = 'đ'
+    if(ignoreChar != d):
+        rr.append(d)
+
+    for _c in rr:
+        x = re.findall(_c, w)
+        if(len(x) < 1):
+            print(w)
+
+
+def findWords(searchText):
+    with open(fileTxt, 'rb', 0) as f, \
          mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ) as s:
         bText = searchText.encode()
         i = s.find(bText)
         z = s.rfind(bText)
-        print(i, z)
+        
         while (i > -1 and i <= z):
-            j = s.find(b'"', i)
-            print(s[i:j].decode("utf-8") )
-            i = s.find(bText, j)
+            end = endRight(s, i)
+            start = beginLeft(s, i, end)
+            word = getWord(s, start, end)
+            #print(sText, word)
+            ignore(word, keyWord)
+            i = s.find(bText, end)
 
-        if i != -1:
-            j = s.find(b'"', i)
-            print(s[i:j].decode("utf-8") )
-            print(j)
-            return True
-    return False
+# def filterWords(text, word):
+#     for _w in word:
+#         x = text.find(_w)
+#         if(x > -1):
+#             print(word)
 
-if check():
-    print('true')
-else:
-    print('false')
-    
+def getWord(s, b, e):
+    return (s[b:e].decode("utf-8"))
+
+def endRight(s, i):
+    j = s.find(b'"', i)
+    # print(s[i:j].decode("utf-8"), j )
+    return j
+
+def beginLeft(s, i, end):
+    j = i - 1
+    while ( -1 < j and j < i):
+        k = s.find(b'"', j)
+        if(k < 0 or k >= end):
+            j -= 1
+        else:
+            break
+    # print(j+1)
+    return j+1
+
+
+findWords(keyWord)
